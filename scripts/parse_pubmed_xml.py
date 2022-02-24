aba0af3997ddef4ec498ef9aaad1b5b5ecead9ed
Keith Hughitt <keith.hughitt@gmail.com>
2022-02-24 07:00:28 -0700

    pubmed date parsing improvements

--- scripts/parse_pubmed_xml.py
+++ scripts/parse_pubmed_xml.py
@@ -47,19 +47,25 @@ for article in root.findall(".//PubmedArticle"):
     doi_elem = article.find(".//ArticleId[@IdType='doi']")
     doi = "" if doi_elem is None else doi_elem.text;
 
-    # extract publication date
-    #  <PubDate>
-    #    <Year>2021</Year>
-    #    <Month>Dec</Month>
-    #    <Day>08</Day>
-    #  </PubDate>
-    date_elem = article.find(".//PubDate")
+    try:
+        date_elem = article.find(".//PubDate")
 
-    year = date_elem[0].text
-    month = date_elem[1].text
-    day = date_elem[2].text
+        year = date_elem[0].text
+        month = date_elem[1].text
 
-    date_str = datetime.strptime(f"{year} {month} {day}", "%Y %b %d").isoformat()
+        # if not day specified, default to the 1st
+        if len(date_elem) == 3:
+            day = date_elem[2].text
+        else:
+            day = "01"
+
+        # check for numeric/string months
+        date_format = "%Y %m %d" if month.isnumeric() else "%Y %b %d"
+
+        date_str = datetime.strptime(f"{year} {month} {day}", date_format).isoformat()
+    except:
+        # if date parsing fails, just leave field blank
+        date_str = ""
 
     ids.append(id)
     dois.append(doi)
